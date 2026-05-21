0e8e7ec5f4553a236f9fe33c6436b288faa9c833
Mike Foster <i.am.mike.foster@gmail.com>
2026-05-21 15:14:04 +0000

    extend examples of Option<Result<!,E>>

--- examples/OptionResult.rs
+++ examples/OptionResult.rs
@@ -1,5 +1,6 @@
 #![feature(never_type)]
 #![feature(try_blocks)]
+#![feature(try_blocks_heterogeneous)]
 use std::io;
 
 fn ignore_blocking(err: io::Error) -> Option<io::Result<!>> {
@@ -17,7 +18,55 @@ pub fn process(input: u32) -> Option<io::Result<u32>> {
     match io_function {
         Ok(_) => Some(io_function),
         Err(e) => ignore_blocking(e) // hopefully in future we can just add a `,` here
-        .map(|e| try {e?}), // currently we need to convert Option<Result<!>> to Option<Result<u32>>
+            .map(|e| try { e? }), // currently we need to convert Option<Result<!>> to Option<Result<u32>>
+    }
+}
+
+pub fn process_some_try(input: u32) -> Option<io::Result<u32>> {
+    let io_function = Ok(input);
+    match io_function {
+        Ok(_) => Some(io_function),
+        Err(e) => {
+            let o: Option<Result<!, io::Error>> = ignore_blocking(e);
+            let r: Result<!, io::Error> = o?; // shorts to a Option<Result<!, io::Error>>::None, which DOES coerce ...
+            Some(try { r? })
+        }
+    }
+}
+
+pub fn process_try_try(input: u32) -> Option<io::Result<u32>> {
+    let io_function = Ok(input);
+    match io_function {
+        Ok(_) => Some(io_function),
+        Err(e) => {
+            try {
+                let o: Option<Result<!, io::Error>> = ignore_blocking(e);
+                let r: Result<!, io::Error> = o?; // shorts to a Option<Result<!, io::Error>>::None, which DOES coerce ...
+                try { r? }
+            }
+        }
+    }
+}
+
+pub fn process_return_result_qmark(input: u32) -> io::Result<u32> {
+    let io_function = Ok(input);
+    match io_function {
+        Ok(_) => io_function,
+        Err(e) => ignore_blocking(e).unwrap()?,
+    }
+}
+
+pub fn process_return_result_long(input: u32) -> io::Result<u32> {
+    let io_function = Ok(input);
+    match io_function {
+        Ok(_) => io_function,
+        Err(e) => {
+            let o: Option<Result<!, io::Error>> = ignore_blocking(e);
+            let r = o.unwrap();
+            let _b = r?;
+            #[allow(unreachable_code)]
+            _b
+        }
     }
 }
 
